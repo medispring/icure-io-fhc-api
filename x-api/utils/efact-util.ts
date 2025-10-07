39ec0a8f49d97a761a3753d101dd4b5632c04631
Mennechet <maxime.mennechet@gmail.com>
2025-10-07 15:53:35 +0200

    Fixed invoiceItemOptions

--- x-api/utils/efact-util.ts
+++ x-api/utils/efact-util.ts
@@ -395,7 +395,7 @@ function toInvoiceItem(
   invoiceItem.units = invoicingCode.units || 1
   invoiceItem.derogationMaxNumber = getDerogationMaxNumber(invoicingCode.derogationMaxNumber || 0)
   invoiceItem.internshipNihii = invoice.internshipNihii
-  invoiceItem.options = invoice.options
+  invoiceItem.options = invoicingCode.options
 
   return invoiceItem
 }
